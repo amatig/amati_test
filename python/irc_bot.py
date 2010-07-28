a2778452b09c49389730341a8df814bef04e10df
amatig <amatig@gmail.com>
2010-07-28 17:15:51 +0200

    fix

--- python/irc_bot.py
+++ python/irc_bot.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python -O
 
-import socket, re, random
+import sys, socket, re, random
 
 class IrcBot():
     delimeter = "\r\n";
     
-    def __init__(self, nick, server, channel, port = 6667):
+    def __init__(self, nick, realname, password, server, channel, port = 6667):
         self.nick = nick
+        self.realname = realname
+        self.password = password
         self.server = server
         self.channel = channel
         self.port = port
@@ -16,53 +18,59 @@ class IrcBot():
         
         self.send("NICK %s" % self.nick)
         self.send("USER %s %s %s :%s" % (self.nick, 
-                                         self.server,
+                                         self.nick,
                                          self.server, 
-                                         self.nick))
+                                         self.realname))
+        self.send("NS identify %s" % self.password)
         self.send("JOIN %s" % self.channel)
-        self.send("TOPIC %s GdrTest" % self.channel)
+        self.send("CS op %s %s" % self.channel, self.nick)
+        #self.send("TOPIC %s Test" % self.channel)
         
         while 1:
             ricev = self.s.recv(1024)
             print ricev
             
-            user = ""
-            cmd = ""
-            chan = ""
-            msg = ""
-            
-            m = re.search("^:(.*)!(.*)\ (.*)\ (.*)\ :(.*)$", ricev)
-            try:
-                msg = m.group(5)
-                user = m.group(1)
-                cmd = m.group(3)
-                chan = m.group(4)
-            except:
-                m = re.search("^:(.*)!(.*)\ (.*)\ :(.*)$", ricev)
+            if ricev.startswith("PING "):
+                self.send("PONG " + ricev.split()[1])            
+            else:
+                user = ""
+                cmd = ""
+                chan = ""
+                msg = ""
+                
+                m = re.search("^:(.*)!(.*)\ (.*)\ (.*)\ :(.*)$", ricev)
                 try:
+                    msg = m.group(5)
                     user = m.group(1)
                     cmd = m.group(3)
                     chan = m.group(4)
                 except:
-                    pass
-            
-            if ricev.startswith("PING "):
-                self.send("PONG " + ricev.split()[1])            
-            elif cmd == "KICK":
-                self.send("JOIN %s" % self.channel)
-            elif cmd == "JOIN":
-                if user != self.nick:
-                    self.send("PRIVMSG %s :Benvenuto..." % user)
-            elif msg.find("ciao") != -1:
-                self.send("PRIVMSG %s :Sto dormendo..." % user)
-            elif msg.find("vito") != -1:
-                self.send("PRIVMSG %s :Vito e' nei campi" % user)
+                    m = re.search("^:(.*)!(.*)\ (.*)\ :(.*)$", ricev)
+                    try:
+                        user = m.group(1)
+                        cmd = m.group(3)
+                        chan = m.group(4)
+                    except:
+                        pass
+                
+                if cmd == "KICK":
+                    self.send("JOIN %s" % self.channel)
+                elif cmd == "JOIN":
+                    if user != self.nick:
+                        self.send("PRIVMSG %s :Benvenuto..." % user)
+                elif msg.find("ciao") != -1:
+                    if chan != self.nick:
+                        self.send("PRIVMSG %s :Sto dormendo..." % chan)
+                    else:
+                        self.send("PRIVMSG %s :Sto dormendo..." % user)
         
     def send(self, msg):
         self.s.send(msg + self.delimeter)
 
 
 if __name__ == "__main__":
-    IrcBot("gmbot_%s" % random.randint(0, 1000), 
+    IrcBot("game_master",
+           "Game Master",
+           len(sys.argv) > 1 and sys.argv[1] or "",
            "irc.freenode.net", 
-           "#amati2010")
+           "#sleipnir")
