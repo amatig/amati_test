8e8f5af3fae35ef3c325e5b5adf624094681ce20
amatig <amatig@gmail.com>
2010-07-29 20:46:58 +0200

    fix

---
+++ cpp/irc_bot/src/irc_bot.cpp
@@ -0,0 +1,16 @@
+#include "irc_bot.h"
+
+IrcBot::IrcBot(string nick,
+	       string realname,
+	       string password,
+	       string server,
+	       int port,
+	       string channel)
+{
+  /*this.nick = nick;
+  this.realname = realname;
+  this.password = password;
+  this.server = server;
+  this.port = port;
+  this.channel = channel;*/
+}
---
+++ cpp/irc_bot/src/irc_bot.h
@@ -0,0 +1,19 @@
+//#include <iostream>
+//#include <string>
+
+using namespace std;
+
+class IrcBot
+{
+ private:
+  string nick;
+  string realname;
+  string password;
+  string server;
+  int port;
+  string channel;
+  
+ public:
+  IrcBot();
+  //  ~IrcBot();
+};
