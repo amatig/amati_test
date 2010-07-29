a7544e51fc6fc7950d0e3e6a360bac4f0d780ca5
amatig <amatig@gmail.com>
2010-07-29 20:47:18 +0200

    fix

--- cpp/irc_bot/src/main.cpp
+++ cpp/irc_bot/src/main.cpp
@@ -1,5 +1,12 @@
+#include "irc_bot.h"
 
 int main(int argc, char *argv[])
 {
-
+  /*IrcBot bot("game_master", 
+	     "Game Master",
+	     "",
+	     "irc.azzurra.org",
+	     6667
+	     "#zero0");*/
+  return 0;
 }
