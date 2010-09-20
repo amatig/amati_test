a225abb7539c6c19855af8af66d888c18af3d297
amatig <amatig@gmail.com>
2010-09-20 12:30:00 +0200

    fix

--- ruby/npc.rb
+++ ruby/npc.rb
@@ -4,7 +4,7 @@ class Npc
   attr_accessor :name, :descr, :place
   
   def initialize(name)
-    file = File.new("npc/#{name}.xml")
+    file = File.new("config/#{name}.xml")
     doc = REXML::Document.new(file)
     root = doc.elements["npc"]
     @name = name
