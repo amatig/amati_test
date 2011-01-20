b6d90993255d9ba4a4fc939fc7a5d64fa4a735a8
amatig <amatig@gmail.com>
2011-01-20 16:41:10 +0100

    fix

--- ruby/virtual_table/client/game.rb
+++ ruby/virtual_table/client/game.rb
@@ -1,40 +1,47 @@
 require "rubygems"
+require "rubygame"
 require "eventmachine"
-require"rubygame"
+
 include Rubygame
 
-class Game < EventMachine::Connection
-  
-  # attr_accessor :q
+class Game < EventMachine::Connection  
+  attr_reader :running
   
   def initialize
-    @screen = Screen.new([800,600], 0, [Rubygame::HWSURFACE, Rubygame::DOUBLEBUF])
-    @q = Rubygame::EventQueue.new
+    @screen = Screen.new([800,600], 
+                         0, 
+                         [Rubygame::HWSURFACE, Rubygame::DOUBLEBUF])
+    @events = Rubygame::EventQueue.new
+    @running = true
+    #loop
   end
   
-  def run
-    @q.each do |ev|
-      puts ev
-      case ev
-      when Rubygame::QuitEvent
-        Rubygame.quit
-        #return
+  def loop
+    #Thread.new do
+      #while @running
+        @events.each do |ev|
+          case ev
+          when Rubygame::MouseDownEvent
+            send_data "mario\r\n"
+          when Rubygame::QuitEvent
+            Rubygame.quit
+            @running = false
+          end
         end
-    end
+      #end
+    #end
   end
   
-  def receive_data data
+  def receive_data(data)
     puts data
   end
 end
 
+
 EventMachine::run do
   emc = EventMachine::connect("0.0.0.0", 8081, Game)
-  EventMachine.add_periodic_timer(0.5) do
-    emc.send_data "mario\r\n"
-    emc.run
+  EventMachine.add_periodic_timer(0.1) do
+    emc.loop
+    return unless emc.running
   end
 end
-
-#main_game = Game.new
-#main_game.run
