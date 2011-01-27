ebe7832de34920d0a5e67572ae329494c1047c8c
amatig <amatig@gmail.com>
2011-01-27 14:02:47 +0100

    fix card

--- ruby/virtual_table/libs/card.rb
+++ ruby/virtual_table/libs/card.rb
@@ -9,14 +9,39 @@ class Card < VObject
     @deck = deck
     @seed = s
     @num = n
+    @turn = false
   end
   
   def init
     @image = Surface.load("./images/#{@deck}/#{@seed}#{@num}.png")
     @image_lock = Surface.load("./images/lock.png")
+    @image_back = Surface.load("./images/#{@deck}/back1.png")
     @rect = @image.make_rect
     set_pos(@x, @y)
     return self
+  end
+  
+  def menu_actions
+    return [["Gira per te", "action_turn_me"], 
+            ["Gira per tutti", "action_turn_all"]]
+  end
+  
+  def action_turn_me
+    
+  end
+  
+  def action_turn_all
+    @turn = (not @turn)
+  end
+  
+  # Ridefinizione del metodo per il deck.
+  def draw(screen)
+    unless @turn
+      @image_back.blit(screen, @rect)
+    else
+      @image.blit(screen, @rect)
+    end
+    @image_lock.blit(screen, @rect) if @lock
   end
   
 end
