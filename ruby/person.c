03d70e2da824189277394f2d43c012e1bce0657a
amatig <amatig@gmail.com>
2011-02-18 14:32:51 +0100

    ruby c

---
+++ ruby/person.c
@@ -0,0 +1,34 @@
+#include <utmp.h>
+#include <ruby.h>
+
+static VALUE mHuman; // module
+static VALUE cPerson;
+
+static VALUE
+cPerson_initialize(VALUE self)
+{
+  VALUE arr;
+
+  arr = rb_ary_new();
+  rb_iv_set(self, "@arr", arr);
+
+  return self;
+}
+
+static VALUE cPerson_add(VALUE self, VALUE obj)
+{
+  VALUE arr;
+
+  arr = rb_iv_get(self, "@arr");
+  rb_ary_push(arr, obj);
+  return arr;
+}
+
+void
+Init_prova()
+{
+  mHuman = rb_define_module("Human");
+  cPerson = rb_define_class_under(mHuman, "Person", rb_cObject);
+  rb_define_method(cPerson, "initialize", cPerson_initialize, 0);
+  rb_define_method(cPerson, "add", cPerson_add, 1);
+}
