6c5d953535a82cbfac7004cb0fc94acf7d31a4b0
amatig <amatig@gmail.com>
2011-03-29 02:16:35 +0200

    fix

--- java/MemoPuzzle/src/org/anddev/andengine/memopuzzle/SumBox.java
+++ java/MemoPuzzle/src/org/anddev/andengine/memopuzzle/SumBox.java
@@ -6,14 +6,19 @@ import org.anddev.andengine.entity.primitive.Rectangle;
 import org.anddev.andengine.entity.scene.Scene;
 import org.anddev.andengine.entity.scene.background.ColorBackground;
 import org.anddev.andengine.entity.sprite.Sprite;
+import org.anddev.andengine.entity.text.Text;
 import org.anddev.andengine.extension.physics.box2d.PhysicsConnector;
 import org.anddev.andengine.extension.physics.box2d.PhysicsFactory;
 import org.anddev.andengine.extension.physics.box2d.PhysicsWorld;
+import org.anddev.andengine.opengl.font.Font;
 import org.anddev.andengine.opengl.texture.Texture;
 import org.anddev.andengine.opengl.texture.TextureOptions;
 import org.anddev.andengine.opengl.texture.region.TextureRegion;
 import org.anddev.andengine.opengl.texture.region.TextureRegionFactory;
+import org.anddev.andengine.util.HorizontalAlign;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.hardware.SensorManager;
 
 import com.badlogic.gdx.math.Vector2;
@@ -24,6 +29,7 @@ import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 public class SumBox extends Scene {
 	private static final int SUP = 9;
 	private static final int INF = 1;
+	private Font mFont;
 	
 	public SumBox(MemoPuzzle game) {
 		super(1);
@@ -41,6 +47,13 @@ public class SumBox extends Scene {
     	
     	game.getEngine().getTextureManager().loadTexture(tex1); // prende + text con la ,
     	
+    	// font
+    	final Texture tex2 = new Texture(256, 256, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+		final Font font1 = new Font(tex2, Typeface.create(Typeface.DEFAULT, Typeface.BOLD), 64, true, Color.BLACK);
+
+		game.getEngine().getTextureManager().loadTexture(tex2);
+		game.getEngine().getFontManager().loadFont(font1);
+		
         // objects
     	final Rectangle ground = new Rectangle(0, MemoPuzzle.CAMERA_HEIGHT, MemoPuzzle.CAMERA_WIDTH, 2);  		
 		final FixtureDef wallFixtureDef = PhysicsFactory.createFixtureDef(0, 0.5f, 0.5f);
@@ -64,9 +77,11 @@ public class SumBox extends Scene {
         			face1.setColor((float)value/SUP, (float)value/SUP, 1.0f);
         			break;
         	}
-        	final FixtureDef objectFixtureDef = PhysicsFactory.createFixtureDef(1, 0.5f, 0.5f);
-        	final Body body1 = PhysicsFactory.createCircleBody(mPhysicsWorld, face1, BodyType.DynamicBody, objectFixtureDef);
-        	getLastChild().attachChild(face1);        	
+        	final FixtureDef objectFixtureDef = PhysicsFactory.createFixtureDef(0, 0.5f, 0.5f);
+        	final Body body1 = PhysicsFactory.createBoxBody(mPhysicsWorld, face1, BodyType.DynamicBody, objectFixtureDef);
+        	getLastChild().attachChild(face1);
+        	final Text label = new Text(30, 15, font1, Integer.toString(value), HorizontalAlign.CENTER);
+        	face1.attachChild(label);
         	mPhysicsWorld.registerPhysicsConnector(new PhysicsConnector(face1, body1, true, false)); // false updata la rotazione
         }
 	}
