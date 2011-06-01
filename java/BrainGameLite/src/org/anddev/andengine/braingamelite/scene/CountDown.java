0b061c6a6a0111c0a00a281f61f46ced11adad0f
amatig <amatig@gmail.com>
2011-06-02 01:33:30 +0200

    fix

--- java/BrainGameLite/src/org/anddev/andengine/braingamelite/scene/CountDown.java
+++ java/BrainGameLite/src/org/anddev/andengine/braingamelite/scene/CountDown.java
@@ -36,8 +36,8 @@ import org.anddev.andengine.util.MathUtils;
 
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 
 public class CountDown extends MyScene {
 	private int mNum;
