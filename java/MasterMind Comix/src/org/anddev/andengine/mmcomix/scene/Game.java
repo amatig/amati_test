8d83a5fb05d3f3afb337950c8af1878f8e65eb0f
amatig <amatig@gmail.com>
2011-06-04 02:24:49 +0200

    ffix

--- java/MasterMind Comix/src/org/anddev/andengine/mmcomix/scene/Game.java
+++ java/MasterMind Comix/src/org/anddev/andengine/mmcomix/scene/Game.java
@@ -10,12 +10,16 @@ import org.anddev.andengine.entity.modifier.MoveYModifier;
 import org.anddev.andengine.entity.modifier.IEntityModifier.IEntityModifierListener;
 import org.anddev.andengine.entity.scene.background.ColorBackground;
 import org.anddev.andengine.entity.sprite.Sprite;
+import org.anddev.andengine.entity.text.Text;
 import org.anddev.andengine.extra.ExtraScene;
 import org.anddev.andengine.extra.Resource;
 import org.anddev.andengine.input.touch.TouchEvent;
+import org.anddev.andengine.opengl.font.Font;
 import org.anddev.andengine.opengl.texture.region.TextureRegion;
 import org.anddev.andengine.util.MathUtils;
 import org.anddev.andengine.util.modifier.IModifier;
+
+import android.graphics.Color;
 
 public class Game extends ExtraScene {
 	
@@ -34,10 +38,14 @@ public class Game extends ExtraScene {
 	private Sprite mSpriteBird1;
 	private Sprite mSpriteBird2;
 	
+	private Font mFont1;
+	
 	@Override
 	public void createScene() {
 		setBackground(new ColorBackground(0.603921569f, 0.909803922f, 0.337254902f));
     	
+		this.mFont1 = Resource.getFont(512, 512, "akaDylan Plain", 20, 1, Color.WHITE, Color.BLACK);
+		
 		this.mClickColor = Resource.getTexture(64, 64, "color");
 		this.mShadow = Resource.getTexture(64, 32, "shadow");
 		this.mBird1 = Resource.getTexture(64, 64, "bird1");
@@ -59,16 +67,16 @@ public class Game extends ExtraScene {
     	
     	// generate sequence
     	this.mListValue = new LinkedList<Integer>();
-    	//for (int i = 0; i < 4; i++)
-    	//	this.mListValue.add(new Integer(MathUtils.random(0, 5)));
-    	this.mListValue.add(new Integer(4));
+    	for (int i = 0; i < 4; i++)
+    		this.mListValue.add(new Integer(MathUtils.random(0, 5)));
+    	/*this.mListValue.add(new Integer(4));
     	this.mListValue.add(new Integer(1));
     	this.mListValue.add(new Integer(2));
-    	this.mListValue.add(new Integer(4));
+    	this.mListValue.add(new Integer(4));*/
     	
     	// create scene
-		int space_x = 25;
-		int space_y = 50;
+		int space_x = 33;
+		int space_y = 45;
 		
 		for (int i = 0; i < 40; i++) {
 			int x = i % 4;
@@ -79,27 +87,25 @@ public class Game extends ExtraScene {
 			h.attachChild(hf);
 			getChild(ExtraScene.GAME_LAYER).attachChild(h);
 			
+			if (x == 0) {
+				Text num = new Text(h.getX() - 23, h.getY() - 11, this.mFont1, Integer.toString(10 - y));
+				getChild(ExtraScene.EXTRA_GAME_LAYER).attachChild(num);
+			}
+			
 			if (x == 3) {
 				Entity pos = new Entity();
-				pos.setPosition(h.getX() + 120, h.getY() - 8);
+				pos.setPosition(h.getX() + 109, h.getY() - 8);
 				pos.attachChild(new Sprite(0, 0, this.mShadow));
 				pos.attachChild(new Sprite(40, 0, this.mShadow));
 				pos.attachChild(new Sprite(0, 20, this.mShadow));
 				pos.attachChild(new Sprite(40, 20, this.mShadow));
-				getChild(ExtraScene.GAME_LAYER).attachChild(pos);
+				getChild(ExtraScene.EXTRA_GAME_LAYER).attachChild(pos);
 			}
-			
-			registerUpdateHandler(new TimerHandler(5f, false, new ITimerCallback() {
-				@Override
-				public void onTimePassed(TimerHandler pTimerHandler) {
-					
-				}
-			}));
 		}
 		
 		int adjust = 28;
 		int space = 73; 
-		int pos_y = 677;
+		int pos_y = 665;
 		
 		Sprite color1 = new Sprite(adjust + 0 * space, pos_y, this.mClickColor);
 		color1.setColor(this.mColor[0][0], this.mColor[0][1], this.mColor[0][2]);
@@ -127,7 +133,7 @@ public class Game extends ExtraScene {
 		getChild(ExtraScene.GAME_LAYER).attachChild(color5);
 		getChild(ExtraScene.GAME_LAYER).attachChild(color6);
 		
-		this.mCount = 45;
+		this.mCount = 44;
 	}
 	
 	@Override
@@ -148,8 +154,8 @@ public class Game extends ExtraScene {
 			return;
 		}
 		
-		if ((this.mCount + 1) % 5 == 0)
-			this.mCount -= 9;
+		if (this.mCount % 4 == 0)
+			this.mCount -= 8;
 		
 		IEntity color = (IEntity) pTouchArea;
 		IEntity h = getChild(ExtraScene.GAME_LAYER).getChild(this.mCount);
@@ -157,17 +163,17 @@ public class Game extends ExtraScene {
 		Sprite w = new Sprite(27, 10, this.mWorm);
 		w.setColor(color.getRed(), color.getGreen(), color.getBlue());
 		h.getFirstChild().attachChild(w);
-		w.registerEntityModifier(new MoveYModifier(0.3f, w.getY(), w.getY() - 46f));
+		w.registerEntityModifier(new MoveYModifier(0.3f, w.getY(), w.getY() - 44f));
 		
 		this.mCount += 1;
 		checkRow(this.mCount);
 	}
 	
 	private void checkRow(int num) {
-		if ((num + 1) % 5 == 0) {
-			IEntity pos = getChild(ExtraScene.GAME_LAYER).getChild(num);
+		if (num % 4 == 0) {
+			IEntity pos = getChild(ExtraScene.EXTRA_GAME_LAYER).getChild(num / 2 - 1);
 			
-			// check di quelli inseriti per non ripetere ricerca colori
+			// check delle soluzioni
 			boolean check[] = new boolean[4];
 			check[0] = false;
 			check[1] = false;
@@ -177,31 +183,41 @@ public class Game extends ExtraScene {
 			LinkedList<Sprite> slotPos = new LinkedList<Sprite>();
 			
 			for (int i = 0; i < 4; i++) {
+				IEntity w = getChild(ExtraScene.GAME_LAYER).getChild(num - 4 + i).getFirstChild().getFirstChild();
 				float r = this.mColor[this.mListValue.get(i).intValue()][0];
 				float g = this.mColor[this.mListValue.get(i).intValue()][1];
 				float b = this.mColor[this.mListValue.get(i).intValue()][2];
-				IEntity w = getChild(ExtraScene.GAME_LAYER).getChild(num - 4 + i).getFirstChild().getFirstChild();
+				
 				if (w.getRed() == r && w.getGreen() == g && w.getBlue() == b) {
 					slotPos.add(0, this.mSpriteBird2);
 					check[i] = true;
 				}
 			}
+			
 			for (int i = 0; i < 4; i++) {
 				if (check[i] == true) continue;
-				float r = this.mColor[this.mListValue.get(i).intValue()][0];
-				float g = this.mColor[this.mListValue.get(i).intValue()][1];
-				float b = this.mColor[this.mListValue.get(i).intValue()][2];
+				IEntity w = getChild(ExtraScene.GAME_LAYER).getChild(num - 4 + i).getFirstChild().getFirstChild();
+				
 				for (int j = 0; j < 4; j++) {
-					IEntity w = getChild(ExtraScene.GAME_LAYER).getChild(num - 4 + j).getFirstChild().getFirstChild();
-					if ((w.getRed() == r && w.getGreen() == g && w.getBlue() == b) && (check[i] == false)) {
+					if (check[j] == true) continue;
+					
+					float r = this.mColor[this.mListValue.get(j).intValue()][0];
+					float g = this.mColor[this.mListValue.get(j).intValue()][1];
+					float b = this.mColor[this.mListValue.get(j).intValue()][2];
+					if ((w.getRed() == r && w.getGreen() == g && w.getBlue() == b) && (check[j] == false)) {
 						slotPos.add(this.mSpriteBird1);
-						check[i] = true;
+						check[j] = true;
 					}
 				}
 			}
 			
-			for (int i = 0; i < 4; i++)
-				pos.getChild(i).attachChild(slotPos.get(i));
+			for (int i = 0; i < 4; i++) {
+				try {
+					pos.getChild(i).attachChild(slotPos.get(i));
+				} catch (Exception e) {
+					
+				}
+			}
 		}
 	}
 	
