2c60b0958bafe709f12bc1873cc7093494079937
amatig <amatig@gmail.com>
2011-08-12 23:33:15 +0200

    fix

--- python/portale_sport/settings.py
+++ python/portale_sport/settings.py
@@ -103,7 +103,6 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/amati/Code/git.repos/portale_sport/core/templates'
 )
 
 INSTALLED_APPS = (
