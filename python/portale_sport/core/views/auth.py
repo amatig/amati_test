f6e7370d71cb9e13e6153c88963ab799d09bbd70
amatig <amatig@gmail.com>
2011-08-23 23:34:57 +0200

    fix

--- python/portale_sport/core/views/auth.py
+++ python/portale_sport/core/views/auth.py
@@ -4,8 +4,9 @@ from django.shortcuts import render_to_response
 from django.contrib.auth import authenticate, login, logout
 
 from core.forms.login_form import LoginForm
+from django.contrib.auth.forms import UserCreationForm
 
-def login_view(request):
+def login(request):
     err = False
     if request.method == "POST":
         next = request.POST["next"]
@@ -22,6 +23,18 @@ def login_view(request):
         form = LoginForm()
     return render_to_response("login.html", {"form": form, "next": next, "err": err}, RequestContext(request))
 
-def logout_view(request):
+def logout(request):
     logout(request)
     return HttpResponseRedirect("/")
+
+def register(request):
+    pageContext = {"title": "Nuovo Utente"}
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/")
+    else:
+        form = UserCreationForm()
+    pageContext.update({"form": form})
+    return render_to_response("form.html", pageContext, RequestContext(request))
