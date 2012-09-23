45174b58ffa70e863f0513598fd5a43ec4c1d386
amatig <amatig@gmail.com>
2012-09-23 21:03:53 +0200

    fix

--- ruby/ScaricaLogo.rb
+++ ruby/ScaricaLogo.rb
@@ -21,21 +21,35 @@ db.execute("SELECT * FROM canali") do |elem|
 	if (not FileTest.exists?(filename)) and elem[5] != nil and elem[5] != ""
 		begin
 			Thread.new do
-				puts "#{_id} #{name}"
+				puts "#{_id} #{name} #{elem[5]}"
 				url = elem[5]
 				resp = Net::HTTP.get_response(URI.parse(url))
-				data = resp.body
+				if resp
+					data = resp.body
 				
-				File.open(filename, 'w') do |f2|  
-  					# use "\n" for two lines of text  
-  					f2.puts data 
+					File.open(filename, 'w') do |f2|  
+  						# use "\n" for two lines of text  
+  						f2.puts data 
+					end
+					
+					%x[convert -resize 50x50 -quality 75 -format PNG images_temp/#{_id} images/#{_id}.png]
+				else
+					puts "NO"
 				end
-				
-				%x[convert -resize 100x100 -quality 75 -format PNG images_temp/#{_id} images/#{_id}.png]
 			end
     	rescue Exception => e2
     		puts "except #{e2}"
     	end
+    else
+    	if ((not FileTest.exists?("images/#{_id}")) && FileTest.exists?(filename))
+			begin
+				Thread.new do
+					%x[convert -resize 50x50 -quality 75 -format PNG images_temp/#{_id} images/#{_id}.png]
+				end
+    		rescue Exception => e2
+    			puts "except #{e2}"
+    		end
+		end
 	end
 	
 end
